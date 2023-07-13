4945019082c52886755f2c9fe768363935600ace
Moura <fot.moura@gmail.com>
2023-07-12 22:51:43 -0300

    first commit

---
+++ src/main/java/com/loginsimple/LoginsimpleApplication.java
@@ -0,0 +1,13 @@
+package com.loginsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginsimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginsimpleApplication.class, args);
+	}
+
+}
