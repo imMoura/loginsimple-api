c3090b4f061bcda88e0e1f508ae9b7bc1eee1d8d
Moura <fot.moura@gmail.com>
2023-07-14 00:35:57 -0300

    Configure project

---
+++ backend/src/test/java/com/loginsimple/LoginsimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginsimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
