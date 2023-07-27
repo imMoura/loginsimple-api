c8e41122c0cd8597065a60a771e60b595c4bcd1a
Moura <fot.moura@gmail.com>
2023-07-26 21:31:00 -0300

    Create UserTask

--- backend/src/main/java/com/loginsimple/services/UserService.java
+++ backend/src/main/java/com/loginsimple/services/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.loginsimple.models.User;
-import com.loginsimple.repositories.TaskRepository;
 import com.loginsimple.repositories.UserRepository;
 
 @Service
@@ -15,9 +14,6 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
-	
-	@Autowired
-	private TaskRepository taskRepository;
 	
 	
 	public User findById(Long id) {
@@ -34,7 +30,6 @@ public class UserService {
 		obj.setId(null);
 		
 		obj = this.userRepository.save(obj);
-		this.taskRepository.saveAll(obj.getTasks());
 		return obj;
 	}
 	
